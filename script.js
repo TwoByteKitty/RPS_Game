37464f53f67a1f49114cecd3e4398692b4866927
Christie Burt <cmburt@yahoo.com>
2020-01-29 23:24:45 -0500

    finished game mechanics & display, except for tie display

--- script.js
+++ script.js
@@ -5,16 +5,54 @@ let playerOneWins = 0;
 let playerTwoWins = 0;
 let ties = 0;
 
-const computerChoice = options[Math.floor(Math.random() * options.length)];
+let playerOneChoice;
 
-function setVisibleImage(player, playerChoice) {
-    const playerSelector = `.player${player}Choice`;
-
-    $(playerSelector).toggle(false);
-    $(`${playerSelector}.${playerChoice}`).toggle(true);
-
+function setVisibleImage(playerSelector, playerChoice) {
+    $(`.${playerSelector}`).toggle(false);
+    $(`.${playerSelector}.${playerChoice}`).toggle(true);
 };
 
-$(".rpsChoice").on("click", function(event){
-    setVisibleImage("One", event.currentTarget.value)
+function faceOff(playerOne, playerTwo) {
+    if ((playerOne === "rock" && playerTwo === "scissors") ||
+    (playerOne === "scissors" && playerTwo === "paper") || 
+    (playerOne === "paper" && playerTwo === "rock")) {
+    playerOneWins++;
+    showWinner("playerOne", playerOne)
+  } else if (playerOne === playerTwo) {
+    ties++;
+    showWinner()
+  } else {
+    playerTwoWins++;
+    showWinner("computer", playerTwo)
+  }
+};
+
+function showWinner(winner, img){
+if(winner){
+    setVisibleImage("playerWinnerChoice", img);
+    $("#playerOneWins").html(playerOneWins);
+    $("#playerTwoWins").html(playerTwoWins);
+}else{
+    setVisibleImage("playerWinnerChoice", "tie");
+}
+};
+
+
+
+
+$(".rpsChoice").on("click", function (event) {
+    playerOneChoice = event.currentTarget.value
+    setVisibleImage("playerOneChoice", event.currentTarget.value)
+});
+
+$(".goBtn").on("click", function (event) {
+    const computerChoice = options[Math.floor(Math.random() * options.length)];
+    setVisibleImage("playerTwoChoice", computerChoice)
+    faceOff(playerOneChoice, computerChoice)
+
+});
+
+$(".resetBtn").on("click", function (event) {
+    setVisibleImage("playerOneChoice", "placeholder")
+    setVisibleImage("playerTwoChoice", "placeholder")
 });
