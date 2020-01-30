6fbc1a9aa21149cc9a8e9243e6591af78848c4b8
Christie Burt <cmburt@yahoo.com>
2020-01-29 19:06:21 -0500

    added jquery & imgs for player selection display

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 
-const choices = [ rock, paper, scissors ];
+const options = ['rock', 'paper', 'scissors'];
+
+let playerOneWins = 0;
+let playerTwoWins = 0;
+let ties = 0;
+
+const computerChoice = options[Math.floor(Math.random() * options.length)];
+
+function setVisibleImage(player, playerChoice) {
+    const playerSelector = `.player${player}Choice`;
+
+    $(playerSelector).toggle(false);
+    $(`${playerSelector}.${playerChoice}`).toggle(true);
+
+};
+
+$(".rpsChoice").on("click", function(event){
+    setVisibleImage("One", event.currentTarget.value)
+});
